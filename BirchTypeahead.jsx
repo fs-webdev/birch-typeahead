640a645575abf66fdbafebce6041fe7c8d324b96
Joey Cozza <joeycozza@gmail.com>
2018-11-01 14:26:50 -0600

    ran the wrapper through babel manually

--- BirchTypeahead.jsx
+++ BirchTypeahead.jsx
@@ -1,34 +1,70 @@
-import React from 'react';
-import './birch-typeahead';
+'use strict';
 
-class BirchTypeahead extends React.Component {
-  constructor(props) {
-    super(props);
-    this.myRef = React.createRef();
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+require('./birch-typeahead');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var BirchTypeahead = function (_React$Component) {
+  _inherits(BirchTypeahead, _React$Component);
+
+  function BirchTypeahead(props) {
+    _classCallCheck(this, BirchTypeahead);
+
+    var _this = _possibleConstructorReturn(this, (BirchTypeahead.__proto__ || Object.getPrototypeOf(BirchTypeahead)).call(this, props));
+
+    _this.myRef = _react2.default.createRef();
+    return _this;
   }
 
-  componentDidMount() {
-    const {onOptionSelected, onInput } = this.props;
+  _createClass(BirchTypeahead, [{
+    key: 'componentDidMount',
+    value: function componentDidMount() {
+      var _props = this.props,
+          onOptionSelected = _props.onOptionSelected,
+          onInput = _props.onInput;
 
-    const typeahead = document.createElement('birch-typeahead');
-    this.myRef.current.appendChild(typeahead);
 
-    typeahead.addEventListener('birch-typeahead:input', e => {
-      const setOptions = options => {
-        typeahead.setAttribute('options', JSON.stringify(options));
-      };
-      const showOptions = () => typeahead.showOptions();
-      onInput(e, setOptions, showOptions);
-    });
+      var typeahead = document.createElement('birch-typeahead');
+      this.myRef.current.appendChild(typeahead);
 
-    typeahead.addEventListener('birch-typeahead:selected', e => {
-      onOptionSelected(e);
-    });
-  }
+      typeahead.addEventListener('birch-typeahead:input', function (e) {
+        var setOptions = function setOptions(options) {
+          typeahead.setAttribute('options', JSON.stringify(options));
+        };
+        var showOptions = function showOptions() {
+          return typeahead.showOptions();
+        };
+        onInput(e, setOptions, showOptions);
+      });
 
-  render() {
-    return <div ref={this.myRef} />;
-  }
-}
+      typeahead.addEventListener('birch-typeahead:selected', function (e) {
+        onOptionSelected(e);
+      });
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      return _react2.default.createElement('div', { ref: this.myRef });
+    }
+  }]);
 
-export default BirchTypeahead;
+  return BirchTypeahead;
+}(_react2.default.Component);
+
+exports.default = BirchTypeahead;
